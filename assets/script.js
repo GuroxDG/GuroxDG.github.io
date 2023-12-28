a26c8d78cf40ce80954131a1ead6e9a8393fac46
Gurox <93458769+GuroxDG@users.noreply.github.com>
2023-12-27 20:26:31 -0500

    versión 0.0.1

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+// Puedes agregar scripts adicionales según sea necesario
+document.getElementById("contact-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+    // Lógica para enviar el formulario
+    alert("Formulario enviado correctamente");
+});
