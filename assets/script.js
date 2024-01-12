88c14d8a7ad4416daa50470041ce0694c310a9a2
Gurox <93458769+GuroxDG@users.noreply.github.com>
2024-01-12 01:45:03 -0500

    update forms

--- assets/script.js
+++
@@ -1,6 +0,0 @@
-// Puedes agregar scripts adicionales según sea necesario
-document.getElementById("contact-form").addEventListener("submit", function (event) {
-    event.preventDefault();
-    // Lógica para enviar el formulario
-    alert("Formulario enviado correctamente");
-});
